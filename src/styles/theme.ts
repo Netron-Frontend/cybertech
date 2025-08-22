59f973455a417a1b8c0bd3a114e5c19c4371d80d
Ivan <ivanmakarevic3@gmail.com>
2025-08-22 15:11:26 +0300

    test_2: theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+	colors: {},
+	breakpoints: {
+		mobile: '768px',
+		tablet: '1024px',
+		desktop: '1200px'
+	},
+	spacing: {
+		xs: '4px',
+		sm: '8px',
+		md: '16px',
+		lg: '24px',
+		xl: '32px'
+	}
+};
+
+export type Theme = typeof theme;
