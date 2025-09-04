db37dcb0440023e3bbe52ad450b46044c7c1723e
Ivan <ivanmakarevic3@gmail.com>
2025-09-04 08:54:24 +0300

    test start backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {HttpModule} from "@nestjs/axios";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entites/user.entity';
+
+
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'CyberTech_db',
+  entities: [User],
+  synchronize: true,
+  
+})
 
 @Module({
-  imports: [],
+  imports: [HttpModule.register({
+    timeout: 5000,
+    maxRedirects: 5,
+  }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import {HttpService} from "@nestjs/axios";
+import {Observable} from "rxjs";
+import {AxiosResponse} from "axios";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+    constructor(private readonly httpService: HttpService) {}
+  
+    NotFoundForm(): Observable<AxiosResponse<any>> {
+    return this.httpService.get('http://localhost:3001/NotFoundForm');
   }
 }
